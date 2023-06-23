d71fdad1c14b710da91f4d8c8f0e4a8f64c39666
Anirudh Thakur <anirudh.thakur94@gmail.com>
2023-06-23 17:13:39 -0500

    css changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';//#import './index.css';
+import React from "react";
 function App() {
   return (
     <div className="App">
